305a5aa188b9de1eea0bc983bc7cf756dfab0b7d
Ed Burtnieks <edburtnieks@gmail.com>
2019-04-18 01:55:05 +0200

    Added accessability test, fixed versioning for eslint and babel

--- src/index.js
+++ src/index.js
@@ -4,4 +4,9 @@ import App from './App';
 import 'normalize.css';
 import './styles/main.scss';
 
+if (process.env.NODE_ENV !== 'production') {
+  const axe = require('react-axe');
+  axe(React, ReactDOM, 1000);
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
